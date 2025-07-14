d6f793dad1255547af4ecb61c878cc3ae6ba64d0
Deadsynthesis34 <ratsirahonanaromeo@gmail.com>
2025-07-14 12:04:32 +0200

    aide function

--- INC/fonction.php
+++ INC/fonction.php
@@ -5,7 +5,8 @@ function dbconnect()
 {
     static $connect = null;
     if ($connect == null) {
-        $connect = mysqli_connect('localhost', 'ETU004317', 'eFR4GqMz', 'db_s2_ETU004317');
+        //$connect = mysqli_connect('localhost', 'ETU004317', 'eFR4GqMz', 'db_s2_ETU004317');
+        $connect = mysqli_connect('localhost', 'root', '', 'db_s2_ETU004317');
         if (!$connect){
             die(''. mysqli_connect_error());
         }
